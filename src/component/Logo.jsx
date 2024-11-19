f6a4f12c70a1a0cc58f87ad01a4b9dab5ade40c6
Lucas <shinono963@gmail.com>
2024-11-19 23:58:59 +0800

    feat: change icon

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -1,19 +1,13 @@
 // material-ui
-import { useTheme } from '@mui/material/styles';
-import { Box, Typography } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
+import { Box } from '@mui/material';
+import LogoIcon from 'assets/images/logo.jpg';
 
 // ==============================|| LOGO COMPONENT ||============================== //
 
 const Logo = () => {
-    const theme = useTheme();
-
     return (
         <Box display="flex" alignItems="center" width="auto" height="32px">
-            <HomeIcon style={{ color: theme.palette.primary.main, fontSize: '32px' }} />
-            <Typography variant="h6" style={{ marginLeft: '8px', color: theme.palette.grey[900] }}>
-                Software Engineering
-            </Typography>
+            <img src={LogoIcon} alt="Logo" style={{ height: '32px', width: 'auto' }} />
         </Box>
     );
 };
